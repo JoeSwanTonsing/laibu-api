619f26e7460f75d9457b1996c91a47d87f9ab216
Suan Tonsing <suanasdf@gmail.com>
2020-10-10 01:44:54 +0530

    created authors api

---
+++ api/authors/index.php
@@ -0,0 +1,31 @@
+<!-- returns all genres -->
+<?php
+  include('../../init.php');
+
+  $response = array();
+
+  $limit = '';
+	if(isset($_GET ['limit'])){
+		$val=$_GET ['limit'];
+		$val=preg_replace('[ˆ0-9]','1',$val);
+		$limit=' LIMIT ' . $val;
+	}
+
+  $sql="SELECT id, name, image, about FROM author".$limit;
+
+  $result = $con->query($sql);
+
+  if ($result->num_rows>0) {
+
+  	while($rows = $result->fetch_assoc()) {
+
+  		$id = $rows['id'];
+  		$name = $rows['name'];
+  		$image = $rows['image'];
+      $about = $rows['about'];
+      array_push($response,array("id"=>$id,"name"=>$name,"image"=>$image,"about"=>$about));
+
+  	}
+  }
+  echo json_encode($response);
+?>
